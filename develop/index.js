89860fd784dadbb346b5e3acdce1f23a639e256a
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-09-14 16:07:18 -0400

    test

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Hey hi ho",
+        name: "random"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
